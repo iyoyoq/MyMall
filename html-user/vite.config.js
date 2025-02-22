cfa61b59a93c720dadaef1f969d9da89e078853b
yoyoq <static2000@163.com>
2025-02-22 22:19:44 +0800

    html-user 引入arco.design 排除mp

--- html-user/vite.config.js
+++ html-user/vite.config.js
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { vitePluginForArco } from '@arco-plugins/vite-vue'
 import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(), vitePluginForArco({
+      style: 'css',
+    })],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  }
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 })
