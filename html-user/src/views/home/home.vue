67ffa8e9ca0cd958189b0b59c2c372b5e1a4da40
iyoyoq <1802360441@qq.com>
2025-02-28 23:03:28 +0800

    改为上部导航栏

--- html-user/src/views/home/home.vue
+++ html-user/src/views/home/home.vue
@@ -6,13 +6,13 @@
           <div class="company-name">{{ companyName }}</div>
         </div>
         <div style="width: 100%">
-          <a-menu mode="horizontal" :default-collapsed=false :default-selected-keys="['0']">
-            <a-menu-item key="0">商城首页</a-menu-item>
-            <a-menu-item key="1">我的收藏</a-menu-item>
-            <a-menu-item key="2">购物车</a-menu-item>
-            <a-menu-item key="3">订单管理</a-menu-item>
-            <a-menu-item key="4">地址管理</a-menu-item>
-            <a-menu-item key="5">个人中心</a-menu-item>
+          <a-menu mode="horizontal" :default-selected-keys="[activeKey]">
+            <a-menu-item key="0" @click="onClickMenuItem('0')">商城首页</a-menu-item>
+            <a-menu-item key="1" @click="onClickMenuItem('1')">我的收藏</a-menu-item>
+            <a-menu-item key="2" @click="onClickMenuItem('2')">购物车</a-menu-item>
+            <a-menu-item key="3" @click="onClickMenuItem('3')">订单管理</a-menu-item>
+            <a-menu-item key="4" @click="onClickMenuItem('4')">地址管理</a-menu-item>
+            <a-menu-item key="5" @click="onClickMenuItem('5')">个人中心</a-menu-item>
           </a-menu>
         </div>
       </a-layout-header>
@@ -26,28 +26,56 @@
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue'
-import { useRouter } from 'vue-router'
+import { defineComponent, ref, onMounted, watch } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
 
 export default defineComponent({
-  setup () {
+  setup() {
     const router = useRouter()
+    const route = useRoute()
     const companyName = ref('骑虎网络技术有限公司')
+    const activeKey = ref('0')
+
+    // 路由路径和菜单键值的映射
+    const pathKeyMap = {
+      '/products': '0',
+      '/favorites': '1',
+      '/cart': '2',
+      '/orders': '3',
+      '/address': '4',
+      '/profile': '5'
+    }
+
+    // 菜单键值和路由路径的映射
+    const keyPathMap = {
+      '0': '/products',
+      '1': '/favorites',
+      '2': '/cart',
+      '3': '/orders',
+      '4': '/address',
+      '5': '/profile'
+    }
+
+    // 根据当前路由设置活动菜单项
+    const setActiveKeyFromRoute = () => {
+      activeKey.value = pathKeyMap[route.path] || '0'
+    }
+
+    // 监听路由变化
+    watch(() => route.path, setActiveKeyFromRoute)
+
+    // 组件挂载时设置初始活动菜单项
+    onMounted(() => {
+      setActiveKeyFromRoute()
+    })
 
     const onClickMenuItem = (key) => {
-      const routeMap = {
-        '0': '/',
-        '1': '/favorites',
-        '2': '/cart',
-        '3': '/orders',
-        '4': '/address',
-        '5': '/profile',
-      }
-      router.push(routeMap[key])
+      router.push(keyPathMap[key])
     }
 
     return {
       companyName,
+      activeKey,
       onClickMenuItem,
     }
   },
