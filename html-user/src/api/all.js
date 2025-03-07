f3a0d7f7ef095be32872157c0b93e9792206731a
iyoyoq <1802360441@qq.com>
2025-03-07 22:41:18 +0800

    api.js

---
+++ html-user/src/api/all.js
@@ -0,0 +1,24 @@
+import request from '@/utils/request.js'
+
+// 示例
+export function demoApi () {
+  return request({
+    url: '/demo',
+    method: 'post',
+    data: {},
+    params: {},
+  })
+}
+
+export function sendLoginCodeApi (phone) {
+  return request({
+    url: '/auth/user/login/sendLoginCode',
+    method: 'get',
+    params: phone,
+  })
+}
+
+
+
+
+
