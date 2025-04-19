d61c924ecefc54ff49c9f671a76fc17688b26576
iyoyoq <1802360441@qq.com>
2025-04-19 10:03:31 +0800

    fix: cors

--- server/src/main/java/com/server/config/request/MvcConfig.java
+++ server/src/main/java/com/server/config/request/MvcConfig.java
@@ -1,9 +1,7 @@
 package com.server.config.request;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -14,31 +12,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
-    // @Autowired
-    // private UserCheckInterceptor userCheckInterceptor;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5174")
-                .allowedOrigins("*") // 报错 allowCredentials 冲突
+                .allowedOrigins("http://localhost:5173", "http://localhost:5174")
+                .allowCredentials(true)
                 .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
-                // .allowCredentials(true)
                 .maxAge(3600);
     }
 
-    // @Override
-    // public void addInterceptors(InterceptorRegistry registry) {
-    //     registry.addInterceptor(userCheckInterceptor)
-    //             .addPathPatterns("/**")
-    //             .excludePathPatterns("/auth/user/login/**")
-    //             // .excludePathPatterns("/admin/**")
-    //             // .excludePathPatterns("/upload/**")
-    //     ;
-    //
-    //     // registry.addInterceptor(new AdminCheckInterceptor())
-    //     //         .addPathPatterns("/admin/**")
-    //     //         .excludePathPatterns("/admin/auth/**");
-    // }
 
 }
